d87b91fc55584da8149b111478b5316060ee2487
Abidaviru <dlrfdnwkd@naver.com>
2019-07-15 20:43:21 +0900

    apply webponent setting

---
+++ Webponent/src/main/webapp/webponent.licenseKey.js
@@ -0,0 +1,18 @@
+/**
+ * 웹포넌트 제품군 라이센스키
+ *
+ * 이 TRIAL버전은 2019년 8월 7일까지 사용하실 수 있습니다.
+ */
+
+
+/**
+ * WEBPONENT_CHART_LICENSE_KEY 웹포넌트 차트의 라이센스키
+ * @type {String}
+ */
+var WEBPONENT_CHART_LICENSE_KEY = "0697063015651%3B%3BKYIgFq%3BIxkTB%3Bzmfxnymj%3BQNBfghbIchxfg%3B2GA";
+
+/**
+ * WEBPONENT_GRID_LICENSE_KEY 웹포넌트 그리드의 라이센스키
+ * @type {String}
+ */
+var WEBPONENT_GRID_LICENSE_KEY = "0697063015651%3B%3BKYfgiB%3BfSkTq%3BLmIxMyNH%3BbNqIghQfAJxfW%3B2dc";
